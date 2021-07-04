6b179777a4b9d7117e638f8eb8e78ff3cb1eff95
chenliang <chenliangj2ee@163.com>
2021-07-05 00:58:15 +0800

    MVVM组件化轻量级框架

--- Component-A/src/main/java/com/chenliang/component_a/act/MainActivity.kt
+++ Component-A/src/main/java/com/chenliang/component_a/act/MainActivity.kt
@@ -3,10 +3,9 @@ package com.chenliang.component_a.act
 import com.chenliang.baselibrary.annotation.MVVM
 import com.chenliang.baselibrary.base.MyBaseActivity
 import com.chenliang.component_a.R
-import com.chenliang.component_a.databinding.AActivityMainBinding
 
-@MVVM(id = R.layout.a_activity_main)
-class MainActivity : MyBaseActivity<AActivityMainBinding>() {
+@MVVM(id=R.layout.base_layout_empty)
+class MainActivity : MyBaseActivity<AAci>() {
     override fun initCreate() {
     }
 
